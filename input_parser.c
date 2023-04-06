f57a31eca6886a80267232a753784165c7b192b9
danimart <danimart@student.42madrid.com>
2023-04-06 18:09:57 +0200

    Always treat philo info as pointer

--- input_parser.c
+++ input_parser.c
@@ -6,31 +6,31 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 17:36:24 by danimart          #+#    #+#             */
-/*   Updated: 2023/04/06 17:53:19 by danimart         ###   ########.fr       */
+/*   Updated: 2023/04/06 18:06:16 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	verify_info(t_philo_info info)
+int	verify_info(t_philo_info *info)
 {
 	int	errors;
 
 	errors = 0;
-	if (info.amount <= 0 || info.amount > MAX_PHILOSOPHERS)
+	if (info->amount <= 0 || info->amount > MAX_PHILOSOPHERS)
 	{
 		printf(AMOUNT_ERR, MAX_PHILOSOPHERS);
 		errors++;
 	}
-	else if (info.amount > 200)
+	else if (info->amount > 200)
 		print_error(AMOUNT_WARN, 1);
-	if (info.die_time <= 0)
+	if (info->die_time <= 0)
 		errors += print_error(DIE_TIME_ERR, 1);
-	if (info.eat_time <= 0)
+	if (info->eat_time <= 0)
 		errors += print_error(EAT_TIME_ERR, 1);
-	if (info.sleep_time <= 0)
+	if (info->sleep_time <= 0)
 		errors += print_error(SLEEP_TIME_ERR, 1);
-	if (info.eat_num <= 0)
+	if (info->eat_num <= 0)
 		errors += print_error(EAT_NUM_ERR, 1);
 	return (errors);
 }
@@ -76,7 +76,7 @@ t_philo_info	*parse_arguments(int argc, char **argv)
 	else
 		info.eat_num = 1;
 	res = &info;
-	if (verify_info(info) != 0)
+	if (verify_info(res) != 0)
 		return (NULL);
 	return (res);
 }
