9bc938e692e2999e37da5b39c553b59569477654
danimart <danimart@student.42madrid.com>
2023-10-06 20:20:40 +0200

    Fix threads checking for non-initialized mutexes

--- philo_builder.c
+++ philo_builder.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 18:55:03 by danimart          #+#    #+#             */
-/*   Updated: 2023/10/06 18:18:24 by danimart         ###   ########.fr       */
+/*   Updated: 2023/10/06 20:13:57 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ t_philo	*buid_philo(int id)
 	philo->l_fork = 0;
 	philo->state = THINKING;
 	philo->meals = 0;
-	philo->th_id = create_philo_thread(philo, &errors);
 	philo->m_state = mutex_init(&errors);
 	philo->m_fork = mutex_init(&errors);
 	philo->m_meal = mutex_init(&errors);
@@ -76,6 +75,13 @@ t_philo_info	*build_philosophers(t_philo_info *info)
 		philo->last_meal = get_current_ms(info);
 		philo->prog_info = info;
 		info->philo_lst[id] = philo;
+		id++;
+	}
+	id = 0;
+	while (id < info->amount)
+	{
+		philo = info->philo_lst[id];
+		philo->th_id = create_philo_thread(philo, NULL);
 		id++;
 	}
 	return (info);
--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 16:31:15 by danimart          #+#    #+#             */
-/*   Updated: 2023/10/06 19:35:52 by danimart         ###   ########.fr       */
+/*   Updated: 2023/10/06 19:45:15 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,15 @@ void	*free_info(char *err, t_philo_info *info, void *result)
 	{
 		id = 0;
 		info->valid = 0;
-		pthread_mutex_destroy(info->m_print);
 		while (id < info->amount && info->philo_lst[id] != NULL)
 		{
+			pthread_detach(info->philo_lst[id]->th_id);
 			pthread_mutex_destroy(info->philo_lst[id]->m_state);
 			pthread_mutex_destroy(info->philo_lst[id]->m_fork);
 			pthread_mutex_destroy(info->philo_lst[id]->m_meal);
-			pthread_detach(info->philo_lst[id]->th_id);
 			id++;
 		}
+		pthread_mutex_destroy(info->m_print);
 		free(info);
 	}
 	return (result);
@@ -62,11 +62,12 @@ void	leaks(void)
 	system("leaks philo | grep 'leaks for' | awk '$1 == \"Process\" {leaks = $3} END {printf(\"\e[0;31mLeaks\e[1;30m: \e[0;33m%d\e[0m\\n\"), leaks}'");
 }
 
-int	print_end_msg(char *msg, pthread_mutex_t *m_print, u_int64_t ms, int id)
+int	print_end_msg(char *msg, t_philo_info *info, u_int64_t ms, int id)
 {
-	pthread_mutex_lock(m_print);
+	pthread_mutex_lock(info->m_print);
 	printf(msg, ms, id);
-	pthread_mutex_unlock(m_print);
+	pthread_mutex_unlock(info->m_print);
+	free_info("End print test\n", info, NULL);
 	return (0);
 }
 
@@ -83,9 +84,9 @@ int	watcher_routine(t_philo_info *info)
 		philo = info->philo_lst[id];
 		pthread_mutex_lock(philo->m_meal);
 		if (info->eat_num > 0 && philo->meals >= info->eat_num)
-			return (print_end_msg(PHILO_SURVIVED, info->m_print, c_ms, id));
+			return (print_end_msg(PHILO_SURVIVED, info, c_ms, id));
 		else if ((get_current_ms(info) - philo->last_meal) > info->die_time)
-			return (print_end_msg(PHILO_DIED, info->m_print, c_ms, id));
+			return (print_end_msg(PHILO_DIED, info, c_ms, id));
 		pthread_mutex_unlock(philo->m_meal);
 		id++;
 	}
@@ -96,7 +97,7 @@ int	main(int argc, char **argv)
 {
 	t_philo_info	*info;
 
-	//atexit(leaks);
+	atexit(leaks);
 	if (DEBUG)
 		printf("\n"DEBUG_NOTE);
 	info = print_info(parse_arguments(argc, argv));
@@ -106,7 +107,7 @@ int	main(int argc, char **argv)
 	if (info == NULL)
 		return (2);
 	while (watcher_routine(info))
-		usleep(100);
+		usleep(1000);
 	free_info(NULL, info, NULL);
 	return (0);
 }
