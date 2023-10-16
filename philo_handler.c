5ecfb9887e8d1a9f920c86f9e2b66f5067196194
danimart <danimart@student.42madrid.com>
2023-10-16 18:57:52 +0200

    Simplified philo_handler

--- philo_handler.c
+++ philo_handler.c
@@ -6,13 +6,13 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 17:54:31 by danimart          #+#    #+#             */
-/*   Updated: 2023/10/16 01:43:35 by danimart         ###   ########.fr       */
+/*   Updated: 2023/10/16 18:57:02 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/philosophers.h"
 
-void	*print_fork_taken(char *msg, t_philo *philo, pthread_mutex_t *m_print)
+void	*print_fork_taken(char *msg, t_philo *philo)
 {
 	int	dead;
 
@@ -21,34 +21,30 @@ void	*print_fork_taken(char *msg, t_philo *philo, pthread_mutex_t *m_print)
 	mutex_unlock(philo->m_ended, 0);
 	if (dead)
 		return (NULL);
-	mutex_lock(m_print);
+	mutex_lock(philo->prog_info->m_print);
 	printf(msg, get_current_ms(philo->prog_info), philo->id + 1);
-	mutex_unlock(m_print, 0);
+	mutex_unlock(philo->prog_info->m_print, 0);
 	return (philo);
 }
 
-void	*take_forks_l(t_philo *philo, t_philo *left)
+void	*take_forks(t_philo *philo, t_philo *left)
 {
-	pthread_mutex_t	*m_print;
-
-	m_print = philo->prog_info->m_print;
-	lock_fork(left);
-	if (print_fork_taken(PHILO_TAKE_LFORK, philo, m_print) == NULL)
-		return (NULL);
-	lock_fork(philo);
-	return (print_fork_taken(PHILO_TAKE_RFORK, philo, m_print));
-}
-
-void	*take_forks_r(t_philo *philo, t_philo *left)
-{
-	pthread_mutex_t	*m_print;
-
-	m_print = philo->prog_info->m_print;
-	lock_fork(philo);
-	if (print_fork_taken(PHILO_TAKE_RFORK, philo, m_print) == NULL)
-		return (NULL);
-	lock_fork(left);
-	return (print_fork_taken(PHILO_TAKE_LFORK, philo, m_print));
+	if (philo->id % 2 == 0)
+	{
+		lock_fork(philo);
+		if (print_fork_taken(PHILO_TAKE_RFORK, philo) == NULL)
+			return (NULL);
+		lock_fork(left);
+		return (print_fork_taken(PHILO_TAKE_LFORK, philo));
+	}
+	else
+	{
+		lock_fork(left);
+		if (print_fork_taken(PHILO_TAKE_LFORK, philo) == NULL)
+			return (NULL);
+		lock_fork(philo);
+		return (print_fork_taken(PHILO_TAKE_RFORK, philo));
+	}
 }
 
 void	*p_eat(t_philo *philo)
@@ -59,14 +55,8 @@ void	*p_eat(t_philo *philo)
 		left = philo->prog_info->philo_lst[philo->prog_info->amount - 1];
 	else
 		left = philo->prog_info->philo_lst[(philo->id - 1)];
-	if (philo->id % 2 == 0)
-	{
-		if (take_forks_r(philo, left) == NULL)
-			return (NULL);
-	}
-	else
-		if (take_forks_l(philo, left) == NULL)
-			return (NULL);
+	if (take_forks(philo, left) == NULL)
+		return (NULL);
 	mutex_lock(philo->m_meal);
 	philo->meals += 1;
 	philo->last_meal = get_current_ms(philo->prog_info);
