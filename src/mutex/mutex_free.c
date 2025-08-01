4959c7e0bac5ddb63ba0068ee3e5c7046466d4cb
xDec0de <xdec0deyt@gmail.com>
2025-08-01 20:31:24 +0200

    Fix mutex_free

--- src/mutex/mutex_free.c
+++ src/mutex/mutex_free.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 15:52:25 by daniema3          #+#    #+#             */
-/*   Updated: 2025/07/31 19:12:24 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/08/01 20:28:42 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 bool	mutex_free(pthread_mutex_t *mutex)
 {
+	bool	success;
+
+	success = true;
 	if (mutex == NULL)
 		return (true);
-	if (pthread_mutex_unlock(mutex) != 0)
-		return (false);
-	err = pthread_mutex_destroy(mutex);
-	if (err != 0)
-		return (false);
+	if (pthread_mutex_destroy(mutex) != 0)
+		success = false;
 	free(mutex);
-	return (true);
+	return (success);
 }
